c5df79607aa707196fe80f596ce6234c30ba7d7b
Razin Syakib <muhammadrazin14@gmail.com>
2023-07-09 13:49:57 +0700

    penjelasan script slider rec dan tombol plus minus

--- pages/Non all u can it/script.js
+++ pages/Non all u can it/script.js
@@ -1,57 +1,78 @@
 // rekomendasi-slider
 $(document).ready(function () {
   var multipleCardCarousel = document.querySelector("#carouselExampleControls");
+
+  // Memeriksa apakah lebar jendela layar sesuai dengan media query "(min-width: 576px)"
   if (window.matchMedia("(min-width: 576px)").matches) {
+    // Jika lebar jendela layar memenuhi media query, maka slider multiple card akan diinisialisasi menggunakan Bootstrap Carousel
     var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      // Menonaktifkan otomatis penggantian slide
       interval: false,
+      // Tidak mengulang slide ketika mencapai slide terakhir atau pertama
       wrap: false,
     });
+
+    // Menghitung lebar total carousel dan lebar setiap item carousel
     var carouselWidth = $(".carousel-inner")[0].scrollWidth;
     var cardWidth = $(".carousel-item").width();
     var scrollPosition = 0;
+
+    // Event handler untuk tombol next pada carousel
     $("#carouselExampleControls .carousel-control-next").on(
       "click",
       function () {
+        // Memeriksa apakah masih ada item slide yang dapat digeser ke kanan
         if (scrollPosition < carouselWidth - cardWidth * 3) {
+          // Menggeser posisi scroll ke kanan sejauh lebar satu item slide
           scrollPosition += cardWidth;
           $("#carouselExampleControls .carousel-inner").animate(
             { scrollLeft: scrollPosition },
-            600
+            600 // Durasi animasi 600ms
           );
         }
       }
     );
+
+    // Event handler untuk tombol prev pada carousel
     $("#carouselExampleControls .carousel-control-prev").on(
       "click",
       function () {
+        // Memeriksa apakah masih ada item slide yang dapat digeser ke kiri
         if (scrollPosition > 1) {
+          // Menggeser posisi scroll ke kiri sejauh lebar satu item slide
           scrollPosition -= cardWidth;
           $("#carouselExampleControls .carousel-inner").animate(
             { scrollLeft: scrollPosition },
-            600
+            600 // Durasi animasi 600ms
           );
         }
       }
     );
   } else {
+    // Jika lebar jendela layar tidak memenuhi media query, maka slider multiple card akan diinisialisasi menggunakan Bootstrap Carousel
     $(multipleCardCarousel).addClass("slide");
   }
 });
 
-// .button-tambah
-const plus = document.querySelector(".plus"),
-  minus = document.querySelector(".minus"),
-  num = document.querySelector(".num");
-let a = 1;
+// Script untuk tombol tambah dan kurang
+const plus = document.querySelector(".plus"), // Tombol tambah
+  minus = document.querySelector(".minus"), // Tombol kurang
+  num = document.querySelector(".num"); // Element angka
+let a = 1; // Variabel angka awal
+
+// Event handler untuk tombol tambah
 plus.addEventListener("click", () => {
-  a++;
-  a = a < 10 ? + a : a;
-  num.innerText = a;
+  a++; // Menambahkan nilai a
+  a = a < 10 ? +a : a; // Mengecek apakah a kurang dari 10, jika ya maka angka 0 akan dihilangkan
+  num.innerText = a; // Mengubah teks pada elemen angka
 });
+
+// Event handler untuk tombol kurang
 minus.addEventListener("click", () => {
+  // Mengecek apakah a lebih dari 1, jika ya maka nilai a akan dikurangi
   if (a > 1) {
-    a--;
-    a = a < 10 ? + a : a;
-    num.innerText = a;
+    a--; // Mengurangi nilai a
+    a = a < 10 ? +a : a; // Mengecek apakah a kurang dari 10, jika ya maka angka 0 akan dihilangkan
+    num.innerText = a; // Mengubah teks pada elemen angka
   }
 });
--- pages/manager/script.js
+++ pages/manager/script.js
@@ -1,13 +1,23 @@
+// Script untuk memfilter menu-manajer
 $(document).ready(function () {
+
+  // Ketika elemen dengan class "manajer-card" diklik, fungsi click() akan dijalankan
   $(".manajer-card").click(function () {
     $(this).addClass("active").siblings().removeClass("active");
+
+    // Mengambil nilai atribut "data-filter" dari elemen yang diklik
     var filter = $(this).attr("data-filter");
+
+    // Jika filter bernilai "all", maka semua menu-manajer akan ditampilkan
     if (filter == "all") {
       $(".manajer-menu-card").show(0);
     } else {
+      // Menyembunyikan menu-manajer yang tidak sesuai dengan filter yang dipilih
       $(".manajer-menu-card")
         .not("." + filter)
         .hide(0);
+
+      // Menampilkan menu-manajer yang sesuai dengan filter yang dipilih
       $(".manajer-menu-card")
         .filter("." + filter)
         .show(0);
