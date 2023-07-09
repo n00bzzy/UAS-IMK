0b6bd31f81ef91c3d9d30c7a30c0f49fa0383c9a
Razin Syakib <muhammadrazin14@gmail.com>
2023-07-09 14:19:08 +0700

    Fungsi untuk mengubah ketersediaan di manajer

--- pages/manager/script.js
+++ pages/manager/script.js
@@ -1,6 +1,5 @@
 // Script untuk memfilter menu-manajer
 $(document).ready(function () {
-
   // Ketika elemen dengan class "manajer-card" diklik, fungsi click() akan dijalankan
   $(".manajer-card").click(function () {
     $(this).addClass("active").siblings().removeClass("active");
@@ -24,3 +23,19 @@ $(document).ready(function () {
     }
   });
 });
+
+// Fungsi untuk mengubah ketersediaan
+function toggleAvailability() {
+  var button = document.getElementById("toggleButton"); // Mengambil elemen tombol dengan ID "toggleButton"
+  var text = button.innerHTML; // Mengambil teks yang ada pada tombol
+
+  if (text === "Tersedia") {
+    // Jika teks pada tombol adalah "Tersedia"
+    button.innerHTML = "Tidak Tersedia"; // Mengubah teks tombol menjadi "Tidak Tersedia"
+    button.style.backgroundColor = "#FF5858"; // Mengubah warna latar belakang tombol menjadi merah
+  } else {
+    // Jika teks pada tombol bukan "Tersedia"
+    button.innerHTML = "Tersedia"; // Mengubah teks tombol menjadi "Tersedia"
+    button.style.backgroundColor = "#44FB04"; // Mengubah warna latar belakang tombol menjadi hijau
+  }
+}
